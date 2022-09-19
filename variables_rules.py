f834985fcac57783052c1eabb99577efe5a7edb2
Shainaz <102401756+shainy786@users.noreply.github.com>
2022-09-19 01:56:20 -0700

    Create variables_rules.py

---
+++ variables_rules.py
@@ -0,0 +1,26 @@
+# a variable start with lowercase or upper case  
+SHAINAZ=22         
+print(SHAINAZ) 
+shainaz=22          
+print(shainaz)
+# a variable start with underscore at start,middle,end in variable
+_shainy=12
+shai_ny=12
+shainy_=12
+print(_shainy)
+print(shai_ny)
+print(shainy_)
+#a varible cannot start with neumaric but we can use at middle or end
+shn786=12
+s786hn=12
+print(shn786)
+print(s786hn)
+#variable size is not fixed
+abcdefghijklmnopqrstuvwxyz=1
+print(abcdefghijklmnopqrstuvwxyz)
+#dont use space in between variables
+# trdtxc gdtd=12                       #space in between variable
+#we can reuse variables
+a=12
+a=42
+print(a)         #updated value will be print-42
